aa0f70b90b10bc1ae81f5f3355339f9bdce7f99e
123 <qeqw@eqwdas.com>
2021-03-12 14:36:19 +0300

    polyfill focus-visible

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,7 @@ function styles() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/focus-visible/dist/focus-visible.min.js',
         'src/js/main.js'
     ])
     .pipe(concat('main.min.js'))
